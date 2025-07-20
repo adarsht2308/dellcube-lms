11397c27a7e8fb518ae41edee607d1473d4a3e19
Aditya Thakur <adityathakurbusiness@gmail.com>
2025-07-20 14:54:34 +0530

    build command updated

--- render-build.sh
+++ render-build.sh
@@ -5,12 +5,3 @@ npm install
 
 # Ensure Puppeteer downloads Chromium
 npx puppeteer install
-
-# Puppeteer cache handling
-if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
-  echo "Copying Puppeteer Cache from Build Cache..." 
-  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
-else 
-  echo "Storing Puppeteer Cache in Build Cache..." 
-  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
-fi
